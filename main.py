361c1586443c7382d7a1a2380ef81b544f88ea88
Rocael14 <jc.vmayen@udeo.edu.gt>
2025-08-06 10:46:36 -0600

    Definimos el menu

---
+++ main.py
@@ -0,0 +1,8 @@
+def menu():
+    print("--- MENU ---")
+    print("1. Ingrese Repartidor")
+    print("2. Ordenar repartidores")
+    print("3. Buscar Repartidor")
+    print("4. Estadisticas")
+    print("5. Salir")
+
